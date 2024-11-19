56956beaca068bfeb5692dcf30fb806d87dd54c2
Garry Red <you@example.com>
2024-11-19 20:42:33 +0100

    update env

--- install_mod.ps1
+++ install_mod.ps1
@@ -1,8 +1,14 @@
-$ModDeployPath = "mods\Telekinesis"
+$ModDeployPath = "c:\Mods"
 
-Copy-Item -v    "deploy\Data\F4SE\Plugins\*.dll" "$ModDeployPath\F4SE\Plugins\"
-Copy-Item -v -r "deploy\Data\F4SE\Plugins\Lovebug\Triggers\*.json" "$ModDeployPath\F4SE\Plugins\Lovebug\Triggers\"
-Copy-Item -v -r "deploy\Data\F4SE\Plugins\Lovebug\Actions\*.json" "$ModDeployPath\F4SE\Plugins\Lovebug\Actions\"
-Copy-Item -v -r "deploy\Data\F4SE\Plugins\Lovebug\Patterns\*.funscript" "$ModDeployPath\F4SE\Plugins\Lovebug\Patterns\"
-Copy-Item -v -r "deploy\Data\Scripts\*.pex" "$ModDeployPath\Scripts"
-Copy-Item -v -r "deploy\Data\Scripts\Source\User\*.psc" "$ModDeployPath\Scripts\Source\User\"
+Copy-Item -v    "deploy\Data\F4SE\Plugins\*.dll" "$ModDeployPath\F4SE\Plugins\" -Force
+Copy-Item -v    "deploy\Data\F4SE\Plugins\Telekinesis2\*.json" "$ModDeployPath\F4SE\Plugins\Telekinesis2\" -Force
+Copy-Item -v -r "deploy\Data\F4SE\Plugins\Telekinesis2\Triggers\*.json" "$ModDeployPath\F4SE\Plugins\Telekinesis2\Triggers\" -Force
+Copy-Item -v -r "deploy\Data\F4SE\Plugins\Telekinesis2\Actions\*.json" "$ModDeployPath\F4SE\Plugins\Telekinesis2\Actions\" -Force
+Copy-Item -v -r "deploy\Data\F4SE\Plugins\Telekinesis2\Races\*.json" "$ModDeployPath\F4SE\Plugins\Telekinesis2\Races\" -Force
+Copy-Item -v -r "deploy\Data\F4SE\Plugins\Telekinesis2\Variables\*.json" "$ModDeployPath\F4SE\Plugins\Telekinesis2\Variables\" -Force
+Copy-Item -v -r "deploy\Data\F4SE\Plugins\Telekinesis2\Patterns\*.funscript" "$ModDeployPath\F4SE\Plugins\Telekinesis2\Patterns\" -Force
+Copy-Item -v -r "deploy\Data\Scripts\*.pex" "$ModDeployPath\Scripts\" -Force
+Copy-Item -v -r "deploy\Data\Scripts\TK2\*.pex" "$ModDeployPath\Scripts\TK2\" -Force
+Copy-Item -v -r "deploy\Data\Scripts\Source\User\TK2\*.psc" "$ModDeployPath\Scripts\Source\User\TK2\" -Force
+Copy-Item -v -r "deploy\Data\MCM\Config\Telekinesis\config.json" "$ModDeployPath\MCM\Config\Telekinesis\config.json" -Force
+Copy-Item -v -r "deploy\Data\MCM\Config\Telekinesis\settings.ini" "$ModDeployPath\MCM\Config\Telekinesis\settings.ini" -Force
